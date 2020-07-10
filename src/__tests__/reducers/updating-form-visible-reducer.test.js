4d3d5e09510eaaddaaed219a7f036909757f93b4
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-10 10:10:36 -0700

    add test for addPotion action, pass, adjust test formatting

--- src/__tests__/reducers/updating-form-visible-reducer.test.js
+++ src/__tests__/reducers/updating-form-visible-reducer.test.js
@@ -4,9 +4,9 @@ import * as c from "./../../actions/ActionTypes";
 describe("updatingFormVisibleReducer", () => {
   test("returns default state if action type is null", () => {
     expect(updatingFormVisibleReducer(false, { type: null })).toEqual(false);
-  })
+  });
 
   test('toggles update form state between false and true', () => {
     expect(updatingFormVisibleReducer(false, { type: c.TOGGLE_UPDATE_FORM })).toEqual(true);
-  })
-})
+  });
+});

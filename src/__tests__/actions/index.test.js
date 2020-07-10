7b040d3de0fc5acccc877bb69679385734bb8cea
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-10 10:10:36 -0700

    add test for addTicet action, pass, adjust test formatting

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,16 +1,46 @@
 import * as a from "./../../actions/index";
 import * as c from "./../../actions/ActionTypes";
 
-describe('crafty brew actions', () => {
-  test('toggleCreateForm should create a TOGGLE_CREATE_FORM aciton', () => {
+import Purple from "./../../img/Purple.gif";
+
+describe("crafty brew actions", () => {
+  const potionData = {
+    name: "Potion of Water Breathing",
+    price: 5,
+    duration: "3:00",
+    effect: "Prevents the oxygen bar from depleting when underwater.",
+    stock: 12,
+    img: Purple,
+    order: 6,
+    key: 6,
+    id: "6"
+  }
+
+  test("toggleCreateForm should create a TOGGLE_CREATE_FORM action", () => {
     expect(a.toggleCreateForm()).toEqual({
       type: c.TOGGLE_CREATE_FORM
     });
   });
 
-  test('toggleUpdateForm should create a TOGGLE_UPDATE_FORM aciton', () => {
+  test("toggleUpdateForm should create a TOGGLE_UPDATE_FORM action", () => {
     expect(a.toggleUpdateForm()).toEqual({
       type: c.TOGGLE_UPDATE_FORM
     });
   });
+
+  test("addPotion should create an ADD_POTION action", () => {
+    expect(a.addPotion(potionData)).toEqual({
+      type: c.ADD_POTION,
+      name: "Potion of Water Breathing",
+      price: 5,
+      duration: "3:00",
+      effect: "Prevents the oxygen bar from depleting when underwater.",
+      stock: 12,
+      img: Purple,
+      order: 6,
+      key: 6,
+      id: "6"
+    });
+  });
+
 });

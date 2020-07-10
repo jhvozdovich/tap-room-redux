77c576b4a70393b6913db571abc34c88aa3cee76
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-10 09:17:06 -0700

    add test for updating form visible action, pass

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -5,6 +5,12 @@ describe('crafty brew actions', () => {
   test('toggleCreateForm should create a TOGGLE_CREATE_FORM aciton', () => {
     expect(a.toggleCreateForm()).toEqual({
       type: c.TOGGLE_CREATE_FORM
-    })
-  })
-})
+    });
+  });
+
+  test('toggleUpdateForm should create a TOGGLE_UPDATE_FORM aciton', () => {
+    expect(a.toggleUpdateForm()).toEqual({
+      type: c.TOGGLE_UPDATE_FORM
+    });
+  });
+});

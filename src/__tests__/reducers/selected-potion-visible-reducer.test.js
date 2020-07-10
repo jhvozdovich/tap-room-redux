3fb388a9aea809e384216292590e2daea34a1fad
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-10 14:04:06 -0700

    add test for selectPotion toggle, pass

--- src/__tests__/reducers/selected-potion-visible-reducer.test.js
+++ src/__tests__/reducers/selected-potion-visible-reducer.test.js
@@ -1,6 +1,5 @@
 import selectedPotionReducer from "./../../reducers/selected-potion-visible-reducer";
 import * as a from "./../../actions/index";
-import * as c from "./../../actions/ActionTypes"
 
 import Purple from "./../../img/Purple.gif";
 
@@ -20,7 +19,7 @@ describe("selectedPotionReducer", () => {
 
   test("returns default without recognized action type", () => {
     expect(selectedPotionReducer("off", { type: null })).toEqual("off");
-  })
+  });
 
   test("should select potion based on input", () => {
     action = a.selectPotion({
@@ -33,7 +32,7 @@ describe("selectedPotionReducer", () => {
       order: 0,
       key: 0,
       id: "0"
-    })
+    });
     expect(selectedPotionReducer("off", action)).toEqual({
       name: "Potion of Regeneration",
       price: 4,
@@ -44,12 +43,11 @@ describe("selectedPotionReducer", () => {
       order: 0,
       key: 0,
       id: "0"
-    })
-  })
+    });
+  });
 
   test("should toggle to null if potion is already selected", () => {
-    action = a.selectPotion(currentState);
-    console.log("__________" + selectedPotionReducer(currentState, action))
-    expect(selectedPotionReducer(currentState, action).toEqual("off"));
-  })
-})
+    action = a.selectPotion(currentState)
+    expect(selectedPotionReducer(currentState, action)).toEqual("off");
+  });
+});

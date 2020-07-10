b8c6ee3e44cf9f682b9a09bdb6532d027f67c22e
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-07-10 09:47:27 -0700

    add test for potion list reducer default, pass

--- src/__tests__/reducers/creating-form-visible-reducer.test.js
+++ src/__tests__/reducers/creating-form-visible-reducer.test.js
@@ -1,5 +1,5 @@
-import creatingFormVisibleReducer from "./../../reducers/creating-form-visible-reducer";
-import * as c from "./../../actions/ActionTypes";
+import creatingFormVisibleReducer from "../../reducers/creating-form-visible-reducer";
+import * as c from "../../actions/ActionTypes";
 
 describe("creatingFormVisibleReducer", () => {
   test("returns default state if action type is null", () => {
---
+++ src/__tests__/reducers/potion-list-reducer.test.js
@@ -0,0 +1,85 @@
+import potionListReducer from "../../reducers/potion-list-reducer";
+import * as a from "./../../actions/index";
+import * as c from "./../../actions/ActionTypes";
+
+import Indigo from "./../../img/Indigo.gif";
+import Purple from "./../../img/Purple.gif";
+import Pink from "./../../img/Pink.gif";
+import LightBlue from "./../../img/LightBlue.gif";
+import Lavendar from "./../../img/Lavendar.gif";
+import PaleGreen from "./../../img/PaleGreen.gif";
+
+describe("potionListReducer", () => {
+
+  const defaultData = {
+    0: {
+      name: "Potion of Regeneration",
+      price: 4,
+      duration: "0:45",
+      effect: "Restores 18 health over time",
+      stock: 10,
+      img: Purple,
+      order: 0,
+      key: 0,
+      id: "0"
+    },
+    1: {
+      name: "Potion of Night Vision",
+      price: 6,
+      duration: "3:00",
+      effect: "Visually brightens everything to a light level of 15",
+      stock: 5,
+      img: Indigo,
+      order: 1,
+      key: 1,
+      id: "1"
+    },
+    2: {
+      name: "Potion of Fire Resistance",
+      price: 8,
+      duration: "3:00",
+      effect: "Gives immunity to damage from fire, lava, magma blocks, campfires, and blazes' ranged attacks.",
+      stock: 15,
+      img: Pink,
+      order: 2,
+      key: 2,
+      id: "2"
+    },
+    3: {
+      name: "Potion of Swiftness",
+      price: 3,
+      duration: "3:00",
+      effect: "Increases movement speed, sprinting speed, and jumping length by 20%.",
+      stock: 0,
+      img: LightBlue,
+      order: 3,
+      key: 3,
+      id: "3"
+    },
+    4: {
+      name: "Potion of Invisibility",
+      price: 10,
+      duration: "3:00",
+      effect: "Renders the player invisible.",
+      stock: 12,
+      img: Lavendar,
+      order: 4,
+      key: 4,
+      id: "4"
+    },
+    5: {
+      name: "Potion of Luck",
+      price: 2,
+      duration: "5:00",
+      effect: "Increases the luck attribute by 1 point.",
+      stock: 1,
+      img: PaleGreen,
+      order: 5,
+      key: 5,
+      id: "5"
+    }
+  }
+  test("returns default state if no action type is passed to reducer", () => {
+    expect(potionListReducer(defaultData, { type: null })).toEqual(defaultData);
+  })
+})
